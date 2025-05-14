d4f30b43c2ca8d28553004a9d3aacfdc814d2c4c
Korenyk.A.N <korenyuk.a.n@mail.ru>
2025-05-14 14:30:36 +0500

    Version 0.2.12
    Механика более удобной иннициализации.

---
+++ machine_tools_3/cli.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------------------------------------------------
+import click
+
+from machine_tools_3.app.db.init_db import init_db_from_csv
+
+
+@click.group()
+def main():
+    """Machine Tools CLI"""
+    pass
+
+
+@main.command()
+def init():
+    """Инициализирует базу данных"""
+    init_db_from_csv()
+
+
+if __name__ == "__main__":
+    main()
