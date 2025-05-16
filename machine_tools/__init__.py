ac3b641be833e78ab7beff7cb623a1417c77aee6
Korenyk.A.N <korenyuk.a.n@mail.ru>
2025-05-16 15:12:02 +0500

    Version 0.2.22
    добавил импорты для удобства

--- machine_tools/__init__.py
+++ machine_tools/__init__.py
@@ -3,10 +3,12 @@
 # ---------------------------------------------------------------------------------------------------------------------
 from machine_tools.app.models.machine import Machine
 from machine_tools.app.models.technical_requirement import TechnicalRequirement
+from machine_tools.app.schemas.machine import MachineInfo
 
-__version__ = "0.1.28"
+__version__ = "0.2.22"
 
 __all__ = [
     "Machine",
+    "MachineInfo",
     "TechnicalRequirement",
 ]
